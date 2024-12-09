09e1b5d21c585c5bd8c6550eed6ea20d46758f45
munrojm <jmunro@lbl.gov>
2024-12-09 09:30:45 -0500

    Fix return statement

--- src/atomicds/core/utils.py
+++ src/atomicds/core/utils.py
@@ -40,9 +40,7 @@ def boxes_overlap(box1, box2) -> bool:
     xmin2, ymin2, xmax2, ymax2 = box2
 
     # Check for overlap
-    if xmax1 < xmin2 or xmax2 < xmin1 or ymax1 < ymin2 or ymax2 < ymin1:
-        return False
-    return True
+    return not (xmax1 < xmin2 or xmax2 < xmin1 or ymax1 < ymin2 or ymax2 < ymin1):
 
 
 def regions_horizontal_overlapping(
